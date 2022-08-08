cff06d616c5fa8b29ac3ca772ee26c1d9a2100c9
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-08 18:12:17 +0200

    Update.

--- Core/Use_DifferentContentSameName.cs
+++ Core/Use_DifferentContentSameName.cs
@@ -1,20 +1,20 @@
-﻿using DifferentContentSameName;
+﻿//using DifferentContentSameName;
 
-namespace Core
-{
-    public partial class Use_DifferentContentSameName
-    {
-        [DifferentContentSameName] private int q = 2;
-        [DifferentContentSameName] private int t = 1;
-    }
+//namespace Core
+//{
+//    public partial class Use_DifferentContentSameName
+//    {
+//        [DifferentContentSameName] private int q = 2;
+//        [DifferentContentSameName] private int t = 1;
+//    }
 
-    public class UsageOf_DifferentContentSameName
-    {
-        public void UseHere()
-        {
-            var name = new Use_DifferentContentSameName();
-            var i = name.T;
-            var nameBig = name.T_Big;
-        }
-    }
-}
+//    public class UsageOf_DifferentContentSameName
+//    {
+//        public void UseHere()
+//        {
+//            var name = new Use_DifferentContentSameName();
+//            var i = name.T;
+//            var nameBig = name.T_Big;
+//        }
+//    }
+//}
