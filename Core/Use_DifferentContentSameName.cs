6b190b88660a34574e44302de8bcbab764dc479e
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-08 17:28:55 +0200

    Add generated file's usages.

--- Core/Use_DifferentContentSameName.cs
+++ Core/Use_DifferentContentSameName.cs
@@ -4,13 +4,17 @@ namespace Core
 {
     public partial class Use_DifferentContentSameName
     {
-        [DifferentContentSameName]private int t = 1;
-        [DifferentContentSameName]private int q = 2;
+        [DifferentContentSameName] private int q = 2;
+        [DifferentContentSameName] private int t = 1;
     }
 
-    public class UsageOf_DifferentContentSameName{ 
-        public void UseHere(){ 
-            new Use_DifferentContentSameName();
-            }
+    public class UsageOf_DifferentContentSameName
+    {
+        public void UseHere()
+        {
+            var name = new Use_DifferentContentSameName();
+            var i = name.T;
+            var nameBig = name.T_Big;
         }
+    }
 }
