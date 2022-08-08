08857428fb1327a57fc927aa57445d6c03d88084
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-08 12:04:25 +0200

    Add SameContentDifferentName.

---
+++ Core/Use_SameContentDifferentName.cs
@@ -0,0 +1,10 @@
+﻿using SameContentDifferentName;
+
+namespace Core
+{
+    public partial class Use_SameContentDifferentName
+    {
+        [SameContentDifferentName] private int t = 1;
+        [SameContentDifferentName] private int t2 = 1;
+    }
+}
